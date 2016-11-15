95596a3d4247948b6dc146f8d00d59c3ca062de1
Ezekiel2000 <ezekiel2000@naver.com>
2016-11-15 20:28:55 +0900

    3day Initial commit

---
+++ d02_intenttest/src/main/java/org/kccc/d02_intenttest/MainActivity.java
@@ -0,0 +1,18 @@
+package org.kccc.d02_intenttest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Log.d("IntentTest", "intent 내용:" + intent.toString());
+    }
+}
