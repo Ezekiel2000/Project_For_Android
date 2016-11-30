d1802d410941f72340fbe802913822c7a89d52c9
Ezekiel2000 <ezekiel2000@naver.com>
2016-11-30 15:56:11 +0900

    7일차

---
+++ d07_mygallery/src/main/java/org/kccc/d07_mygallery/GalleryAdapter.java
@@ -0,0 +1,8 @@
+package org.kccc.d07_mygallery;
+
+/**
+ * Created by Ezekiel on 2016. 11. 30..
+ */
+
+public class GalleryAdapter {
+}
---
+++ d07_recyclerviewtest/src/main/java/org/kccc/d07_recyclerviewtest/RVAdapter.java
@@ -0,0 +1,8 @@
+package org.kccc.d07_recyclerviewtest;
+
+/**
+ * Created by Ezekiel on 2016. 11. 30..
+ */
+
+public class RVAdapter {
+}
