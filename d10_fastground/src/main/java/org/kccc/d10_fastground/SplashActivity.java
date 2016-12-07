866a4777627cfab046067596b3d6f77748406b68
Ezekiel2000 <ezekiel2000@naver.com>
2016-12-07 22:25:03 +0900

    10Day

---
+++ d10_fastground/src/main/java/org/kccc/d10_fastground/SplashActivity.java
@@ -0,0 +1,13 @@
+package org.kccc.d10_fastground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
